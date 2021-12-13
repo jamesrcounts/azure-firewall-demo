e2dfdd54048c2a1ce9105db8a0f2016caa8ed17c
Jim Counts <jamesrcounts@outlook.com>
2021-12-13 22:49:25 +0000

    basic wire up

--- infrastructure/azure/resource-group.tf
+++ infrastructure/azure/resource-group.tf
@@ -1,5 +1,4 @@
-resource "azurerm_resource_group" "main" {
-  name     = "rg-${local.project}"
-  location = "centralus"
-  tags     = local.tags
+# Read details about the resource group created for this project.
+data "azurerm_resource_group" "rg" {
+  name = "rg-${var.env_instance_id}"
 }
---
+++ infrastructure/azure/variables.tf
@@ -0,0 +1,9 @@
+variable "env_instance_id" {
+  description = "(Required) The instance ID for the solution environment."
+  type        = string
+}
+
+variable "env_subscription_id" {
+  description = "(Required) The Azure subscription ID for the solution environment."
+  type        = string
+}
