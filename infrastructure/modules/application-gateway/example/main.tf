b2a1ae4bbfaeede5852afe40f9b2bd26fea710f0
Jim Counts <jamesrcounts@outlook.com>
2021-12-23 19:58:24 +0000

    Fixup example

--- infrastructure/modules/application-gateway/example/main.tf
+++ infrastructure/modules/application-gateway/example/main.tf
@@ -17,7 +17,12 @@ resource "azurerm_resource_group" "test" {
   tags     = local.tags
 }
 
-data "azurerm_client_config" "current" {
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "keyvault_secrets_admin" {
+  scope                = azurerm_resource_group.test.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_key_vault" "example" {
@@ -28,57 +33,7 @@ resource "azurerm_key_vault" "example" {
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   tags                       = local.tags
-
-  // access_policy {
-  //   tenant_id = data.azurerm_client_config.current.tenant_id
-  //   object_id = data.azurerm_client_config.current.object_id
-
-  //   certificate_permissions = [
-  //     "create",
-  //     "delete",
-  //     "deleteissuers",
-  //     "get",
-  //     "getissuers",
-  //     "import",
-  //     "list",
-  //     "listissuers",
-  //     "managecontacts",
-  //     "manageissuers",
-  //     "purge",
-  //     "setissuers",
-  //     "update",
-  //   ]
-
-  //   key_permissions = [
-  //     "backup",
-  //     "create",
-  //     "decrypt",
-  //     "delete",
-  //     "encrypt",
-  //     "get",
-  //     "import",
-  //     "list",
-  //     "purge",
-  //     "recover",
-  //     "restore",
-  //     "sign",
-  //     "unwrapKey",
-  //     "update",
-  //     "verify",
-  //     "wrapKey",
-  //   ]
-
-  //   secret_permissions = [
-  //     "backup",
-  //     "delete",
-  //     "get",
-  //     "list",
-  //     "purge",
-  //     "recover",
-  //     "restore",
-  //     "set",
-  //   ]
-  // }
+  enable_rbac_authorization  = true
 }
 
 resource "azurerm_key_vault_certificate" "example" {
@@ -97,41 +52,41 @@ resource "azurerm_key_vault_certificate" "example" {
       reuse_key  = true
     }
 
-    //   lifetime_action {
-    //     action {
-    //       action_type = "AutoRenew"
-    //     }
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
 
-    //     trigger {
-    //       days_before_expiry = 30
-    //     }
-    //   }
+      trigger {
+        days_before_expiry = 30
+      }
+    }
 
     secret_properties {
       content_type = "application/x-pkcs12"
     }
 
-    //   x509_certificate_properties {
-    //     # Server Authentication = 1.3.6.1.5.5.7.3.1
-    //     # Client Authentication = 1.3.6.1.5.5.7.3.2
-    //     extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+    x509_certificate_properties {
+      # Server Authentication = 1.3.6.1.5.5.7.3.1
+      # Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
 
-    //     key_usage = [
-    //       "cRLSign",
-    //       "dataEncipherment",
-    //       "digitalSignature",
-    //       "keyAgreement",
-    //       "keyCertSign",
-    //       "keyEncipherment",
-    //     ]
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
 
-    //     subject_alternative_names {
-    //       dns_names = ["internal.contoso.com", "domain.hello.world"]
-    //     }
+      subject_alternative_names {
+        dns_names = ["test.contoso.com"]
+      }
 
-    //     subject            = "CN=hello-world"
-    //     validity_in_months = 12
-    //   }
+      subject            = "CN=hello-world"
+      validity_in_months = 12
+    }
   }
 }
 
