43e153ad58ccb3e85d30d729cdce39f8d22a72a0
Jim Counts <jamesrcounts@outlook.com>
2021-12-30 15:40:07 +0000

    perform encoding inside module

--- infrastructure/hub-and-spoke/workstation.tf
+++ infrastructure/hub-and-spoke/workstation.tf
@@ -1,7 +1,7 @@
 module "workstation" {
   source = "../modules/workstation"
 
-  ca_certificate       = base64encode(data.azurerm_key_vault_certificate_data.rootca.pem)
+  ca_certificate       = data.azurerm_key_vault_certificate_data.rootca.pem
   instance_id          = var.env_instance_id
   network_interface_id = module.networks.worker_network_interface.id
   resource_group       = data.azurerm_resource_group.rg
