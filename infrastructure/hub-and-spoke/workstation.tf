bc4377ede37dcc8cb0e677ae307ca7048a66231c
Jim Counts <jamesrcounts@outlook.com>
2021-12-30 16:36:40 +0000

    make workstation wait for appservice rules

--- infrastructure/hub-and-spoke/workstation.tf
+++ infrastructure/hub-and-spoke/workstation.tf
@@ -1,6 +1,10 @@
 module "workstation" {
   source = "../modules/workstation"
 
+  depends_on = [
+    azurerm_firewall_policy_rule_collection_group.appservice_rules
+  ]
+
   ca_certificate       = data.azurerm_key_vault_certificate_data.rootca.pem
   instance_id          = var.env_instance_id
   network_interface_id = module.networks.worker_network_interface.id
