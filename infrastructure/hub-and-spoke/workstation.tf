9df72632d0d2ea04ef2fde418f486f3da66cb402
Jim Counts <jamesrcounts@outlook.com>
2021-12-31 21:36:56 +0000

    Remove rules from firewall module

--- infrastructure/hub-and-spoke/workstation.tf
+++ infrastructure/hub-and-spoke/workstation.tf
@@ -6,6 +6,7 @@ module "workstation" {
   ]
 
   ca_certificate       = data.azurerm_key_vault_certificate_data.rootca.pem
+  firewall_policy_id   = module.firewall.firewall_policy_id
   instance_id          = var.env_instance_id
   network_interface_id = module.networks.worker_network_interface.id
   resource_group       = data.azurerm_resource_group.rg
